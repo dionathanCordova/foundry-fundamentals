478effcd889f33291156ab7451f4c2df1255ed1a
Dionathan Córdova <dionathan.cordova@gmail.com>
2025-01-23 23:43:48 -0300

    feat: iuncluindo o valor alterado dentro da prop logs da transacao

--- src/Counter.sol
+++ src/Counter.sol
@@ -4,11 +4,23 @@ pragma solidity ^0.8.24;
 contract Counter {
     uint256 public number;
 
+    event NumberUpdateLog(uint256 numberLog);
+
     function setNumber(uint256 newNumber) public {
         number = newNumber;
+        emit NumberUpdateLog(number);
     }
 
-    function increment() public {
+    function increment() public returns(uint256) {
         number++;
+        emit NumberUpdateLog(number);
+        return number;
+    }
+
+    function decrement() public returns(uint256) {
+        require(number > 0, "Counter: underflow detected");
+        number--;
+        emit NumberUpdateLog(number);
+        return number;
     }
 }
