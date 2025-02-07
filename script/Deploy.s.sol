f7ba74daca6b6e9306d124a8ee1bfd588692caa3
Dionathan Cordova <dionathan.cordova@gmail.com>
2025-02-06 22:40:00 -0300

    refactor: transfer rolando normalmente

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -18,39 +18,30 @@ contract Deploy is Script {
     // funcao de log que armazena os dadso no evento BalanceUpdated
     event BalanceUpdated(address client, uint256 balance);
 
-    function logBalance(Token token, uint256 i) public {
-        uint256 balanceBefore = token.balanceOf(clients[i]);
-        console2.log("Balance of client before transfer", i, "is: ", balanceBefore); // todo, esta caindo aqui como zerado. verificar
-        
+    function logBalance(Token token, address client) public {
         // Verificar se o contrato tem saldo suficiente para transferir
         uint256 balance = token.balanceOf(address(this));
         require(balance >= amount, "Insufficient contract balance to transfer tokens");
 
         // Transferir tokens
-        token.transfer(clients[i], amount);
+        token.transfer(client, 100);
 
-        // Obter o saldo após a transferência
-        uint256 balanceAfter = token.balanceOf(clients[i]);
-        console2.log("Balance of client after transfer", i, "is: ", balanceAfter);
+        uint256 balanceAfter = token.balanceOf(client);
 
         // Emitir o evento com a informação
-        emit BalanceUpdated(clients[i], balanceAfter);
+        emit BalanceUpdated(client, balanceAfter);
     }
 
     function run() external {
-        vm.startBroadcast(0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d);
+        Token token = new Token(amount);
 
-        Token token = new Token(20000000);
-        console2.log("Contract deployed with address:", address(token));
+        vm.startBroadcast();
 
         for (uint i = 0; i < clients.length; i++) {
-            this.logBalance(token, i);
+            this.logBalance(token,  clients[i]);
         }
 
         vm.stopBroadcast();
     }
-
-    // todo ver pq o deploy está falhando.
-    // todo testar 
 }
 
--- src/Token.sol
+++ src/Token.sol
@@ -3,10 +3,14 @@ pragma solidity ^0.8.24;
 
 import {ERC20} from "lib/solady/src/tokens/ERC20.sol";
 
+import {console2} from "forge-std/console2.sol";
+
 contract Token is ERC20 {
 
     constructor(uint256 _amount) {
         _mint(msg.sender, _amount);
+        console2.log("Deployer address: ", msg.sender);
+        console2.log("Addres ammount: ", balanceOf(msg.sender));
     }
 
     function name() public pure override returns(string memory) {
